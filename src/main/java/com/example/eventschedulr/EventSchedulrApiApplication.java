ea9b52c5278b5342108d8fc30e6e5b89c6ba68f3
Ismael-Moreira-Kt <ismaelmoreiraubuntu@gmail.com>
2024-06-12 22:50:35 +0100

    feat: project initialization

---
+++ src/main/java/com/example/eventschedulr/EventSchedulrApiApplication.java
@@ -0,0 +1,11 @@
+package com.example.eventschedulr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventSchedulrApiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EventSchedulrApiApplication.class, args);
+	}
+}
